8ab14a37ca76b4c4b69b21c3db115c60cfc2d523
huaerze <be_it_so@163.com>
2026-07-16 14:29:03 +0800

    Serve IndexNow key explicitly

--- _worker.js
+++ _worker.js
@@ -7,6 +7,15 @@ export default {
       return Response.redirect(url.toString(), 301);
     }
 
+    if (url.pathname === "/04cb9707b196d4d8b34d0f083fb95f05.txt") {
+      return new Response("04cb9707b196d4d8b34d0f083fb95f05", {
+        headers: {
+          "content-type": "text/plain; charset=utf-8",
+          "cache-control": "public, max-age=300",
+        },
+      });
+    }
+
     return env.ASSETS.fetch(request);
   },
 };
